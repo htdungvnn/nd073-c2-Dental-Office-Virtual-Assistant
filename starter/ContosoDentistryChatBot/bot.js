3736817b7a2cf00a46b7bfb15ca212720efacbee
htdung.vnn <htdung.vnn@gmail.com>
2024-11-14 19:03:06 +0700

    update ptoj

--- starter/ContosoDentistryChatBot/bot.js
+++ starter/ContosoDentistryChatBot/bot.js
@@ -1,59 +1,81 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const { ActivityHandler, MessageFactory } = require('botbuilder');
+const { ActivityHandler, ActivityTypes } = require('botbuilder');
+const { CustomQuestionAnswering } = require('botbuilder-ai');
 
-const { QnAMaker } = require('botbuilder-ai');
-const DentistScheduler = require('./dentistscheduler');
-const IntentRecognizer = require("./intentrecognizer")
-
-class DentaBot extends ActivityHandler {
-    constructor(configuration, qnaOptions) {
-        // call the parent constructor
+class CustomQABot extends ActivityHandler {
+    constructor() {
         super();
-        if (!configuration) throw new Error('[QnaMakerBot]: Missing parameter. configuration is required');
 
-        // create a QnAMaker connector
-        this.QnAMaker = new QnAMaker()
-       
-        // create a DentistScheduler connector
-      
-        // create a IntentRecognizer connector
-
-
-        this.onMessage(async (context, next) => {
-            // send user input to QnA Maker and collect the response in a variable
-            // don't forget to use the 'await' keyword
-          
-            // send user input to IntentRecognizer and collect the response in a variable
-            // don't forget 'await'
-                     
-            // determine which service to respond with based on the results from LUIS //
-
-            // if(top intent is intentA and confidence greater than 50){
-            //  doSomething();
-            //  await context.sendActivity();
-            //  await next();
-            //  return;
-            // }
-            // else {...}
-             
-            await next();
-    });
-
-        this.onMembersAdded(async (context, next) => {
-        const membersAdded = context.activity.membersAdded;
-        //write a custom greeting
-        const welcomeText = '';
-        for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
-            if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
-            }
+        try {
+            this.qnaMaker = new CustomQuestionAnswering({
+                knowledgeBaseId: process.env.ProjectName,
+                endpointKey: process.env.LanguageEndpointKey,
+                host: process.env.LanguageEndpointHostName
+            });
+        } catch (err) {
+            console.warn(`QnAMaker Exception: ${ err } Check your QnAMaker configuration in .env`);
         }
-        // by calling next() you ensure that the next BotHandler is run.
-        await next();
-    });
+
+        // If a new user is added to the conversation, send them a greeting message
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            for (let cnt = 0; cnt < membersAdded.length; cnt++) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    const DefaultWelcomeMessageFromConfig = process.env.DefaultWelcomeMessage;
+                    await context.sendActivity(DefaultWelcomeMessageFromConfig?.length > 0 ? DefaultWelcomeMessageFromConfig : 'Hello and Welcome');
+                }
+            }
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        // When a user sends a message, perform a call to the QnA Maker service to retrieve matching Question and Answer pairs.
+        this.onMessage(async (context, next) => {
+            if (!process.env.ProjectName || !process.env.LanguageEndpointKey || !process.env.LanguageEndpointHostName) {
+                const unconfiguredQnaMessage = 'NOTE: \r\n' +
+                    'Custom Question Answering is not configured. To enable all capabilities, add `ProjectName`, `LanguageEndpointKey` and `LanguageEndpointHostName` to the .env file. \r\n' +
+                    'You may visit https://language.cognitive.azure.com/ to create a Custom Question Answering Project.';
+
+                await context.sendActivity(unconfiguredQnaMessage);
+            } else {
+                console.log('Calling CQA');
+
+                const enablePreciseAnswer = process.env.EnablePreciseAnswer === 'true';
+                const displayPreciseAnswerOnly = process.env.DisplayPreciseAnswerOnly === 'true';
+                const response = await this.qnaMaker.getAnswers(context, { enablePreciseAnswer: enablePreciseAnswer });
+
+                // If an answer was received from CQA, send the answer back to the user.
+                if (response.length > 0) {
+                    var activities = [];
+
+                    var answerText = response[0].answer;
+
+                    // Answer span text has precise answer.
+                    var preciseAnswerText = response[0].answerSpan?.text;
+                    if (!preciseAnswerText) {
+                        activities.push({ type: ActivityTypes.Message, text: answerText });
+                    } else {
+                        activities.push({ type: ActivityTypes.Message, text: preciseAnswerText });
+
+                        if (!displayPreciseAnswerOnly) {
+                            // Add answer to the reply when it is configured.
+                            activities.push({ type: ActivityTypes.Message, text: answerText });
+                        }
+                    }
+                    await context.sendActivities(activities);
+                    // If no answers were returned from QnA Maker, reply with help.
+                } else {
+                    await context.sendActivity('No answers were found.');
+                }
+            }
+
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
     }
 }
 
-module.exports.DentaBot = DentaBot;
+module.exports.CustomQABot = CustomQABot;
--- starter/ContosoDentistryChatBot/index.js
+++ starter/ContosoDentistryChatBot/index.js
@@ -1,107 +1,73 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+// index.js is used to setup and configure your bot
+
+// Import required packages
 const path = require('path');
 
-const dotenv = require('dotenv');
-// Import required bot configuration.
+// Note: Ensure you have a .env file and include QnAMakerKnowledgeBaseId, QnAMakerEndpointKey and QnAMakerHost.
 const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE });
+require('dotenv').config({ path: ENV_FILE });
 
 const restify = require('restify');
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { BotFrameworkAdapter } = require('botbuilder');
+const {
+    ActivityTypes,
+    CloudAdapter,
+    ConfigurationBotFrameworkAuthentication
+} = require('botbuilder');
 
-// This bot's main dialog.
-const { DentaBot } = require('./bot');
+// The bot.
+const { CustomQABot } = require('./bot');
 
-// Create HTTP server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${server.name} listening to ${server.url}`);
-    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
-    console.log('\nTo talk to your bot, open the emulator select "Open Bot"');
-});
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env);
 
 // Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about how bots work.
-const adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword
-});
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 // Catch-all for errors.
-const onTurnErrorHandler = async (context, error) => {
+adapter.onTurnError = async (context, error) => {
+    // Create a trace activity that contains the error object
+    const traceActivity = {
+        type: ActivityTypes.Trace,
+        timestamp: new Date(),
+        name: 'onTurnError Trace',
+        label: 'TurnError',
+        value: `${ error }`,
+        valueType: 'https://www.botframework.com/schemas/error'
+    };
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
-    console.error(`\n [onTurnError] unhandled error: ${error}`);
+    //       application insights. See https://aka.ms/bottelemetry for telemetry
+    //       configuration instructions.
+    console.error(`\n [onTurnError] unhandled error: ${ error }`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${error}`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
-    );
+    await context.sendActivity(traceActivity);
 
     // Send a message to the user
     await context.sendActivity('The bot encountered an error or bug.');
     await context.sendActivity('To continue to run this bot, please fix the bot source code.');
 };
 
-// Set the onTurnError for the singleton BotFrameworkAdapter.
-adapter.onTurnError = onTurnErrorHandler;
-
-// Map configuration values values from .env file into the required format for each service.
-const QnAConfiguration = {
-    knowledgeBaseId: process.env.QnAKnowledgebaseId,
-    endpointKey: process.env.QnAAuthKey,
-    host: process.env.QnAEndpointHostName
-};
-
-const LuisConfiguration = {
-    applicationId: process.env.LuisAppId,
-    endpointKey: process.env.LuisAPIKey,
-    endpoint: process.env.LuisAPIHostName,
-}
-
-const SchedulerConfiguration = {
-    SchedulerEndpoint: process.env.SchedulerEndpoint
-}
-//pack each service configuration into 
-const configuration = {
-    QnAConfiguration,
-    LuisConfiguration,
-    SchedulerConfiguration
-}
-
 // Create the main dialog.
-const myBot = new DentaBot(configuration, {});
+const bot = new CustomQABot();
 
-// Listen for incoming requests.
-server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
-        await myBot.run(context);
-    });
+// Create HTTP server
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log('\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator');
 });
 
-// Listen for Upgrade requests for Streaming.
-server.on('upgrade', (req, socket, head) => {
-    // Create an adapter scoped to this WebSocket connection to allow storing session data.
-    const streamingAdapter = new BotFrameworkAdapter({
-        appId: process.env.MicrosoftAppId,
-        appPassword: process.env.MicrosoftAppPassword
-    });
-    // Set onTurnError for the BotFrameworkAdapter created for each connection.
-    streamingAdapter.onTurnError = onTurnErrorHandler;
-
-    streamingAdapter.useWebSocket(req, socket, head, async (context) => {
-        // After connecting via WebSocket, run this logic for every request sent over
-        // the WebSocket connection.
-        await myBot.run(context);
-    });
+// Listen for incoming activities and route them to your bot main dialog.
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res, (context) => bot.run(context));
 });
